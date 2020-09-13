5a46a659d2567da592513dabc56d43ab8982aa08
Aquarius0715 <ji1wxs@outlook.jp>
2020-09-13 15:17:13 +0900

    説明

--- main.c
+++ main.c
@@ -7,13 +7,25 @@ int main() {
      * 文字を１文字入力すると、それが、大文字か、小文字か、そのどちらでもないかを判定するプログラムを作りなさい。
      */
 
+    /*
+     * 変数宣言
+     */
+
     char chara;
+
+    /*
+     * 文字を取得
+     */
 
     printf("Input character: ");
 
     scanf("%c", &chara);
 
     printf("Character = %c\n", chara);
+
+    /*
+     * 文字が小文字かどうかを判定
+     */
 
     for (char character = 'a'; character <= 'z'; ++character) {
 
@@ -31,6 +43,10 @@ int main() {
 
     }
 
+    /*
+     * 文字が大文字かどうかを判定
+     */
+
     for (char character = 'A'; character <= 'Z'; ++character) {
 
         if (character != chara) {
@@ -46,6 +62,10 @@ int main() {
         }
 
     }
+
+    /*
+     * 大文字でも小文字でもない場合
+     */
 
     printf("Neither\n");
 
